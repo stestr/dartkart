5867cb9b85a244ad6477b789177bfd3ebbba0663
Gubaer <karl.guggisberg@guggis.ch>
2013-05-26 17:45:22 +0200

    integrated tests for MapViewport

--- test/map/test_map.dart
+++ test/map/test_map.dart
@@ -2,8 +2,10 @@ library test_map;
 
 import "test_layer_control.dart" as test_layer_control;
 import "test_pan_behaviour.dart" as test_pan_behaviour;
+import "test_map_viewport.dart" as test_map_viewport;
 
 main() {
   test_layer_control.main();
   test_pan_behaviour.main();
+  test_map_viewport.main();
 }
