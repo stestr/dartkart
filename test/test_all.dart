119afd888e6495c63b200b2bd026d4a275fabb01
Gubaer <karl.guggisberg@guggis.ch>
2013-05-26 14:38:25 +0200

    updated test infrastructure

---
+++ test/test_all.dart
@@ -0,0 +1,16 @@
+
+
+import "core/test_core.dart" as test_core;
+import "geo/test_geo.dart" as test_geo;
+import "geometry/test_geometry.dart" as test_geometry;
+import "layer/test_base.dart" as test_base;
+
+main() {
+  test_core.main();
+  test_geo.main();
+  test_geometry.main();
+  test_base.main();
+}
+
+
+
