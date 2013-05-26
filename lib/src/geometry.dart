f08e857cb3d1f859891a9eb7b5664e922ce73ebc
Gubaer <karl.guggisberg@guggis.ch>
2013-05-26 08:02:17 +0200

    added Point2D and Dimension

--- lib/src/geometry.dart
+++ lib/src/geometry.dart
@@ -5,6 +5,12 @@ import "dart:collection";
 import "dart:async";
 import "dart:json" as json;
 
-part 'geometry/features.dart';
 part 'geometry/bounds.dart';
+part "geometry/point2d.dart";
+part "geometry/dimension.dart";
+
+_require(cond,[msg=""]) {
+  if (cond) return;
+  throw new ArgumentError(msg);
+}
 
---
+++ lib/src/geometry/dimension.dart
@@ -0,0 +1,26 @@
+part of dartkart.geometry;
+
+/**
+ * The dimension of a rectangular area given by a width and a height.
+ */
+class Dimension {
+  /// the width
+  final num width;
+  /// the height
+  final num height;
+
+  /**
+   * Creates a dimension object with [width] and [height].
+   *
+   * Throws [ArgumentError] if either [width] or [height] is
+   * negative.
+   */
+  Dimension(this.width, this.height) {
+    _require(width >=0, "width must be positive");
+    _require(height >=0, "height must be positive");
+  }
+
+  bool operator ==(other) => width == other.width && height == other.height;
+  int get hashCode => width.hashCode * 31 + height.hashCode;
+  String toString() => "{Dimension: width=$width, height=$height}";
+}
